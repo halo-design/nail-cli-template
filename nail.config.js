74f11903da9b190401e5e33940c9076937caface
OwlAford <244734834@qq.com>
2019-05-20 18:54:12 +0800

    ⬆️ Upgrade core modlues

--- nail.config.js
+++ nail.config.js
@@ -1,35 +1,35 @@
 module.exports = () => {
-  const isProd = process.env.NODE_ENV === "production";
-  const prefix = isProd ? "production.min" : "development";
+  const isProd = process.env.NODE_ENV === 'production';
+  const prefix = isProd ? 'production.min' : 'development';
 
   return {
     proxyTable: {
-      "/data": {
-        target: "https://www.example.com",
-        changeOrigin: true
-      }
+      '/data': {
+        target: 'https://www.example.com',
+        changeOrigin: true,
+      },
     },
-    eslintExtend: "prettier",
+    eslintExtend: 'prettier',
     autoOpenBrowser: true,
     lintOnSave: true,
     pwa: true,
     productionSourceMap: false,
     cdn: {
-      prodUrl: "https://cdn.bootcss.com/:name/:version/:path",
+      prodUrl: 'https://cdn.bootcss.com/:name/:version/:path',
       modules: {
         react: [
           {
-            name: "react",
-            var: "React",
-            path: `umd/react.${prefix}.js`
+            name: 'react',
+            var: 'React',
+            path: `umd/react.${prefix}.js`,
           },
           {
-            name: "react-dom",
-            var: "ReactDOM",
-            path: `umd/react-dom.${prefix}.js`
-          }
-        ]
-      }
-    }
+            name: 'react-dom',
+            var: 'ReactDOM',
+            path: `umd/react-dom.${prefix}.js`,
+          },
+        ],
+      },
+    },
   };
 };
